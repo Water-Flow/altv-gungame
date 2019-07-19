572d65f78ca5277126ea10e2ddc7f6862f4f1170
Franze, Mario <mfranze@germandev.de>
2019-07-19 11:43:17 +0200

    * Fixed deprecated callback

--- resources/gungame/client/main.mjs
+++ resources/gungame/client/main.mjs
@@ -1,8 +1,24 @@
 import alt from 'alt';
 import game from 'natives';
 
+let cefOverlay;
+
+alt.onServer('playerReady', () =>
+{
+	cefOverlay = new alt.WebView('http://resources/gungame/client/cef/overlay/overlay.html');
+	cefOverlay.on('AltInWindow', () =>
+	{
+		alt.setTimeout(() => cefOverlay.emit('ResetPlayerData'), 1500);
+	});
+});
+
 alt.onServer('removeAllPedWeapons', () =>
 {
-	const player = alt.getLocalPlayer().scriptID;
+	const player = alt.Player.local.scriptID;
 	game.removeAllPedWeapons(player, true);
+});
+
+alt.onServer('updatePoints', (data) =>
+{
+	cefOverlay.emit('UpdatePlayerData', data);
 });
